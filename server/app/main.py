95f3938d4f5c45fee39ae898a2a7da5ab70eeb71
Arghya Dutta <arghyadutta080@gmail.com>
2025-04-30 12:03:01 +0530

    update: app renaming in api-docs

--- server/app/main.py
+++ server/app/main.py
@@ -5,7 +5,7 @@ from app.routes import user, auth, post
 
 Base.metadata.create_all(bind=engine)
 
-app = FastAPI()
+app = FastAPI(title="Blogi API", version="1.0.0")
 app.include_router(user.router, prefix="/api/v1/user", tags=["Users"])
 app.include_router(auth.router, prefix="/api/v1/auth", tags=["Auth"])
 app.include_router(post.router, prefix="/api/v1/posts", tags=["Posts"])
--- server/app/routes/post.py
+++ server/app/routes/post.py
@@ -13,7 +13,7 @@ router = APIRouter()
 def create_post(
     title: str = File(...),
     content: str= File(...),
-    image: Optional[UploadFile] = File(None),
+    image: Optional[UploadFile] = File(None) | None,
     db: Session = Depends(getDB.get_db),
     current_user=Depends(get_current_user)
 ):
